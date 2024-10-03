a74f633278239dbc19feb89b14cc76df96c8a02e
Muhammad Awaludin Fadli <muhammadawaludinfadli@gmail.com>
2024-10-03 10:36:03 +0700

    Add files via upload

---
+++ Tugas 3 no 1 kanan button.ino
@@ -0,0 +1,35 @@
+const int button1 = 5;  // 
+const int led1 = 4;
+const int led2 = 3;
+const int led3 = 2;
+
+void setup() {
+  // 
+  pinMode(button1, INPUT);
+  
+  // 
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+}
+
+void loop() {
+  // Jika tombol ditekan
+  if (digitalRead(button1) == HIGH) {
+    
+    // Menyalakan LED 1
+    digitalWrite(led1, HIGH);
+    delay(700); // 
+    digitalWrite(led1, LOW); //
+    
+    // Menyalakan LED 2
+    digitalWrite(led2, HIGH);
+    delay(700); // 
+    digitalWrite(led2, LOW); // 
+    
+    // Menyalakan LED 3
+    digitalWrite(led3, HIGH);
+    delay(700); // 
+    digitalWrite(led3, LOW); // 
+  }
+}
